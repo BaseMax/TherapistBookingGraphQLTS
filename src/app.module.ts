7ad8926b85d7720bc9fa7baabea130da1b6de3b4
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-07 14:15:07 +0330

    add graphql and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { TherapistModule } from './therapist/therapist.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot() ,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      context : ({req}) => ({req}) ,
+      driver : ApolloDriver , 
+      autoSchemaFile : join(process.cwd() , "src/schema.gql") , 
+      playground : false , 
+      plugins : [ApolloServerPluginLandingPageGraphQLPlayground()]
+    }),
+    PrismaModule,
+    UserModule,
+    TherapistModule ,
+  ],
+  providers: [],
 })
 export class AppModule {}

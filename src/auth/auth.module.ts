2896cc9c29df49effbfe7f98b7cce4804cf557a2
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-08 22:56:50 +0330

    register passport and jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStartegy } from './jwt/jwt.startegy';
 
 @Module({
-  providers: [AuthResolver, AuthService]
+  imports : [
+    ConfigModule ,
+    PrismaModule , 
+    UserModule , 
+    PassportModule.register({
+      defaultStrategy : 'jwt' ,
+      property : 'user' , 
+      session : false , 
+    }) ,
+    JwtModule.registerAsync({
+      imports : [ConfigModule] , 
+      inject : [ConfigService] , 
+      useFactory : async (configService:ConfigService)=>({
+        secret : configService.get<string>('JWT_SECRET_KEY') , 
+        signOptions : {
+          expiresIn : configService.get<string>('TOKEN_EXPIRATION')
+        }
+      })
+    })
+  ] ,
+  providers: [AuthResolver, AuthService , JwtStartegy] , 
+  exports : [
+    PassportModule , 
+    JwtModule , 
+  ]
 })
 export class AuthModule {}

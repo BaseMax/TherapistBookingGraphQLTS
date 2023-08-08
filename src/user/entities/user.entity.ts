2b9df3bc5bddbf311d8cf0309cb38323237ce339
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-08 19:25:28 +0330

    change fields

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,32 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Appointment, Review, Role, Therapist } from '@prisma/client';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>ID)
+  id : string ;
+
+  @Field(()=>String)
+  firstName : string ;  
+
+  @Field(()=>String)
+  lastName : string ; 
+
+  @Field(()=>String)
+  password : string ;
+  
+  @Field(()=>String)
+  email : string ;
+
+  @Field(()=>[String])
+  roles : Role[]; 
+  
+  @Field(()=>[String])
+  therapists : Therapist[]; 
+  
+  @Field(()=>[String])
+  reviews : Review[] ;
+  
+  @Field(()=>[String])
+  appointments : Appointment[] ;
 }

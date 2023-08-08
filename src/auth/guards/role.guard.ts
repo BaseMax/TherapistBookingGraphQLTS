1ffecdc263a17682cd15eea6398c7036ccd66e86
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-08 22:54:51 +0330

    create jwt auth guard and role gurad

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { Role } from "@prisma/client";
+import { Observable } from "rxjs";
+
+
+@Injectable()
+export class RoleGuard implements CanActivate{
+    constructor(
+        private reflector : Reflector ,
+    ){}
+
+    canActivate(context: GqlExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const ctx = GqlExecutionContext.create(context);
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles',[
+            ctx.getHandler() , 
+            ctx.getClass()
+        ])
+
+        if(!requiredRoles){
+            return true ;
+        }
+
+        const { user } = ctx.getContext().req;
+
+        return requiredRoles.some((role)=>user?.roles?.includes(role));
+    }
+}

9befc82b40a6cb99eb2f30028135c8656c0ecc73
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-09 23:03:55 +0330

    add new resolvers

--- src/therapist/therapist.resolver.ts
+++ src/therapist/therapist.resolver.ts
@@ -1,35 +1,90 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { TherapistService } from './therapist.service';
 import { Therapist } from './entities/therapist.entity';
 import { CreateTherapistInput } from './dto/create-therapist.input';
 import { UpdateTherapistInput } from './dto/update-therapist.input';
+import { GetUser } from 'src/common/decorator/get-user.decorator';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Role } from '@prisma/client';
+import { UseGuards } from '@nestjs/common';
+import { HasRoles } from 'src/common/decorator/hash-roles.decorator';
+import { StatusResult } from 'src/common/entity/status-result.entity';
 
 @Resolver(() => Therapist)
 export class TherapistResolver {
   constructor(private readonly therapistService: TherapistService) {}
 
-  @Mutation(() => Therapist)
-  createTherapist(@Args('createTherapistInput') createTherapistInput: CreateTherapistInput) {
-    return this.therapistService.create(createTherapistInput);
+
+  @UseGuards(JwtAuthGuard)
+  @Query(() => Therapist, { name: 'getTherapist' })
+  getTherapist(@GetUser() user) {
+    return this.therapistService.getTherapist(user.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Therapist], { name: 'findAllTherapist' })
   findAll() {
     return this.therapistService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => Therapist, { name: 'findOneTherapist' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.therapistService.findOne(id);
+  findOne(@Args('id', { type: () => ID}) id: string) {
+    return this.therapistService.findOne({id});
   }
 
-  @Mutation(() => Therapist)
-  updateTherapist(@Args('updateTherapistInput') updateTherapistInput: UpdateTherapistInput) {
-    return this.therapistService.update(updateTherapistInput.id, updateTherapistInput);
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [Therapist], { name: 'findTherapistAvailability' })
+  findTherapistAvailability() {
+    return this.therapistService.findTherapistAvailability();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Query(() => StatusResult , { name: 'getTherapistCount' })
+  getTherapistCount() {
+    return this.therapistService.getTherapistCount();
+  }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [StatusResult] , { name: 'findTherapistsBySpecialty' })
+  findTherapistsBySpecialty(@Args('specialty') specialty:string) {
+    return this.therapistService.findTherapistsBySpecialty(specialty);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Query(() => [StatusResult] , { name: 'findTherapistByLocation' })
+  findTherapistByLocation(@Args('location') location:string) {
+    return this.therapistService.findTherapistByLocation(location);
+  }
+
+
+  @HasRoles(Role.ADMIN , Role.THERAPIST)
+  @UseGuards(JwtAuthGuard , RoleGuard)
+  @Mutation(() => StatusResult , {name : 'updateTherapist'})
+  updateTherapist(
+    @Args('updateTherapistInput') updateTherapistInput: UpdateTherapistInput , 
+    @GetUser() user ) {
+    return this.therapistService.update(user.id, updateTherapistInput);
+  }
+
+
+
+
+  @HasRoles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard , RoleGuard)
   @Mutation(() => Therapist)
-  removeTherapist(@Args('id', { type: () => Int }) id: number) {
+  createTherapist(
+    @Args('createTherapistInput') createTherapistInput: CreateTherapistInput ,
+    ) {
+    return this.therapistService.create(createTherapistInput);
+  }
+
+  @HasRoles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard , RoleGuard)
+  @Mutation(() => StatusResult , {name : 'removeTherapist'})
+  removeTherapist(@Args('id') id: string) {
     return this.therapistService.remove(id);
   }
 }

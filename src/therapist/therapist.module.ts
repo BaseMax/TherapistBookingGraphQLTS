55f57d797015cec725143910d0aa7ebf399845cb
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-09 23:04:50 +0330

    add prisma module

--- src/therapist/therapist.module.ts
+++ src/therapist/therapist.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TherapistService } from './therapist.service';
 import { TherapistResolver } from './therapist.resolver';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports : [UserModule , PrismaModule],
   providers: [TherapistResolver, TherapistService]
 })
 export class TherapistModule {}

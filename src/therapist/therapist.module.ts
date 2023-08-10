8ead9d9ea9c4ff4301afea5903ddb9f0d010accd
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-10 22:12:35 +0330

    Add module

--- src/therapist/therapist.module.ts
+++ src/therapist/therapist.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports : [UserModule , PrismaModule],
-  providers: [TherapistResolver, TherapistService]
+  providers: [TherapistResolver, TherapistService] , 
+  exports : [TherapistService]
 })
 export class TherapistModule {}

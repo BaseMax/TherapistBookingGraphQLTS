5b588ea3423112f201f6b2885e9a77837747fd69
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-10 22:08:16 +0330

    Add PrismaModule and TherapistModule

--- src/appointment/appointment.module.ts
+++ src/appointment/appointment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppointmentService } from './appointment.service';
 import { AppointmentResolver } from './appointment.resolver';
+import { TherapistModule } from 'src/therapist/therapist.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports : [TherapistModule , PrismaModule],
   providers: [AppointmentResolver, AppointmentService]
 })
 export class AppointmentModule {}
